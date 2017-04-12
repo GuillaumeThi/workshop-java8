f357b488d3dcab0518791e0a48392299a44e3a64
Guillaume THIRIET <fake@fake.com>
2017-04-12 11:25:10 +0200

    tests ok

--- src/test/java/java8/ex02/Method_02_Test.java
+++ src/test/java/java8/ex02/Method_02_Test.java
@@ -21,6 +21,11 @@ public class Method_02_Test {
         // TODO créer une méthode String format()
         // TODO la méthode retourne une chaîne de la forme [<nb_personnes> persons]
         // TODO exemple de résultat : "[14 persons]", "[30 persons]"
+        default String format()
+        {
+        	List<Person> people = this.findAll();
+        	return "[" + people.size() + " persons]";
+        }
     }
     // end::IDao[]
 
@@ -38,6 +43,11 @@ public class Method_02_Test {
         // TODO la méthode retourne une chaîne de la forme DaoA[<nb_personnes> persons]
         // TODO exemple de résultat : "DaoA[14 persons]", "DaoA[30 persons]"
         // TODO l'implémentation réutilise la méthode format() de l'interface
+        public String format()
+        {
+        	List<Person> people = this.findAll();
+        	return "DaoA[" + people.size() + " persons]";
+        }
 
     }
     // end::DaoA[]
@@ -48,7 +58,7 @@ public class Method_02_Test {
         DaoA daoA = new DaoA();
 
         // TODO invoquer la méthode format() pour que le test soit passant
-        String result = null;
+        String result = daoA.format();
 
         assertThat(result, is("DaoA[20 persons]"));
     }
